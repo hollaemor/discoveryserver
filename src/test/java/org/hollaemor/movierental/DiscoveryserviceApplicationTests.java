7d7325dc281ac61f34c7330bb2e40288103d1413
Emmanuel Oriarewo <hollaemor@gmail.com>
2020-05-01 01:15:43 +0200

    Setup discovery server

---
+++ src/test/java/org/hollaemor/movierental/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hollaemor.movierental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
